f85eb0761ce250cc10b5e0bc7c2bdb90d379a543
hi-lufz1 <latifufauzi@gmail.com>
2025-05-08 22:30:27 +0700

    menambahkan text untuk bagian selected date

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app_bloc/todo_bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16.0), 
+          padding: EdgeInsets.all(16.0),
           child: Column(
             children: [
               Text('Todo List'),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text("Selected Date"),
+                    ],
+                  ),
+                ],
+              ),
             ],
-          )),
+          ),
+        ),
       ),
     );
   }
