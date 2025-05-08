b8ca5f130fcfc51a0c863f502c4cc7c66003c7bc
hi-lufz1 <latifufauzi@gmail.com>
2025-05-08 22:53:38 +0700

    menambahkan tampilan untuk tanggal yang dipilih

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -19,6 +19,18 @@ class TodoPage extends StatelessWidget {
                   Column(
                     children: [
                       Text("Selected Date"),
+                      BlocBuilder<TodoBloc, TodoState>(
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text("No date selected");
+                        },
+                      ),
                     ],
                   ),
                 ],
