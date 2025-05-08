7cad371f1cee871446c75818481dc8114dc5734b
hi-lufz1 <latifufauzi@gmail.com>
2025-05-08 23:21:22 +0700

    menambahkan key untuk form state dan text controller

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -7,6 +7,8 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
     return Scaffold(
       body: SafeArea(
         child: Padding(
