7378f8bc2c68f61af09223c8098403d7de35015b
hi-lufz1 <latifufauzi@gmail.com>
2025-05-08 23:06:10 +0700

    menambahkan tombol untuk memilih tanggal

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -33,6 +33,13 @@ class TodoPage extends StatelessWidget {
                       ),
                     ],
                   ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Select Date"),
+                    ),
+                  ),
                 ],
               ),
             ],
