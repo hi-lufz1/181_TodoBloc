e8b421dd870844ccf1cd8c596654a9beb8c52808
hi-lufz1 <latifufauzi@gmail.com>
2025-05-09 00:00:42 +0700

    menambahkan tampilan untuk menampilkan status loading dan list todo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -97,6 +97,22 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if (state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (state is TodoLoaded) {
+                      if (state.todos.isEmpty) {
+                        return Center(child: Text('Todo List is empty'));
+                      }
+                    } else {
+                      return Center(child: Text('No todos Available'));
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         ),
