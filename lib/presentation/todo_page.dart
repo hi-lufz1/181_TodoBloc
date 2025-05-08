752a0cf5ee1362cc43cee5743acbcea9f629458f
hi-lufz1 <latifufauzi@gmail.com>
2025-05-08 22:19:21 +0700

    menambahkan text

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -5,6 +5,16 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0), 
+          child: Column(
+            children: [
+              Text('Todo List'),
+            ],
+          )),
+      ),
+    );
   }
 }
